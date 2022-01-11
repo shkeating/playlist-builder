2abd08988570a5d934dc35d7dd5607fe4d640d91
Shauna Keating <shauna@truss.works>
2022-01-11 15:28:11 -0500

    refactors Search Bar component to be more accessibile

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,8 +3,9 @@ import styles from "./index.module.scss";
 const SearchBar = () => {
   return (
     <div className={styles.SearchBar}>
-      <input placeholder="Enter A Song, Album, or Artist" />
-      <button className={styles.SearchButton}>SEARCH</button>
+      <label for="entry">Search song information</label>
+      <input id="entry" placeholder="Enter A Song, Album, or Artist" />
+      <button className={styles.SearchButton}>Search</button>
     </div>
   );
 };
