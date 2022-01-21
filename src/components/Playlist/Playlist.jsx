1e276725b6ed6fe0e0b061334dd5214272c21b8b
Shauna Keating <shauna@truss.works>
2022-01-20 20:23:16 -0500

    creates mixin of search result div styles and applies it to playlist, adjusts playlist styles

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import styles from "./index.module.scss";
 import Track from "../Track/Track";
 
@@ -5,8 +6,24 @@ const Playlist = (props) => {
   const [playlistName, setPlaylistName] = useState("New Playlist");
   return (
     <div className={styles.Playlist}>
-      <input value="New Playlist" />
-      <TrackList />
+      <label htmlFor="playlist-title">Enter Playlist Name:</label>
+      <input
+        onChange={(e) => setPlaylistName(e.target.value)}
+        value={playlistName}
+        id="playlist-title"
+      />
+      <div className="TrackList">
+        {props.tracks.map((track) => {
+          return (
+            <Track
+              key={track.id}
+              track={track}
+              trackActionCharacter="-"
+              handleTrackAction={props.removeTrackFromPlaylist}
+            />
+          );
+        })}
+      </div>
       <button className={styles.PlaylistSave}>Save to Spotify</button>
     </div>
   );
